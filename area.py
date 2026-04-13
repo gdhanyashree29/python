cbca25ee6c16f9148013d1ea031ffd114237e7aa
gdhanyashree29 <gdhanyashree29@gmail.com>
2026-04-13 11:40:31 +0530

    Create area.py

---
+++ area.py
@@ -0,0 +1,16 @@
+num=int(input('enter the value of num:'))
+if(num<2):
+l = float(input('enter the breadth of triangle:'))
+h = float(input('enter the height of triangle:'))
+   area=0.5 * l * h
+   print('area',area)
+else:
+ a=int(input('enter the value of a:'))
+ b=int(input('enter the value of b:'))
+ c=int(input('enter the value of c:'))
+if(a<0 or b<0 or c<0):
+ print("positive value")
+else:
+ s=(a+b+c)\2
+area1=sqrt(s*(s-a)*(s-b)*(s-c))
+print("area1,area)
