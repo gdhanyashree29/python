be587f853cdd0572c6b02f2274cb4ceba2d41449
gdhanyashree29 <gdhanyashree29@gmail.com>
2026-04-13 12:01:53 +0530

    Create fibonacci func.py

---
+++ fibonacci func.py
@@ -0,0 +1,11 @@
+[23bee003@mepcolinux ~]$cat ex4a.py
+def factorial(n):
+    if n < 0:
+        return "Factorial not defined for negative numbers"
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    return fact
+
+num = int(input("Enter a number: "))
+print("Factorial is:", factorial(num))
