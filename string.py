36d95f34cdaa78d239940d73155d85a73d411e12
gdhanyashree29 <gdhanyashree29@gmail.com>
2026-04-20 11:28:25 +0530

    string.py

---
+++ string.py
@@ -0,0 +1,6 @@
+string = raw_input("Enter a string: ")
+length = len(string)
+reversed_string = string[::-1]
+print("Original String:", string)
+print("Length of the string:", length)
+print("Reversed String:", reversed_string)
